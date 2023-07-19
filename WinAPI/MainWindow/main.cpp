77568569b6b6fabc319bf4fd7dac173f51572790
Alikhanov Ibragim N <a01141226993n@gmail.com>
2023-07-19 18:44:55 +0300

    HW_WIN size is DONE

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,7 +1,10 @@
-﻿#include<Windows.h>
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<Windows.h>
 #include"resource.h"
+#include<cstdio>
 
 CONST CHAR g_sz_MY_WINDOW_CLASS[] = "MyFirstWindow";
+
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -48,13 +51,16 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     //2) Создание окна:
     
+    INT newWidth = (INT)GetSystemMetrics(SM_CXSCREEN) * 3 / 4;
+    INT newHeight = (INT)GetSystemMetrics(SM_CYSCREEN) * 3 / 4;
+
     HWND hwnd = CreateWindowEx(
         0,
         g_sz_MY_WINDOW_CLASS,   //Имя класса окна
         g_sz_MY_WINDOW_CLASS,   //Заголовок окна
         WS_OVERLAPPEDWINDOW, //Стиль окна для главного окна программы всегда будет WS_OVERLAPPEDWINDOW - это окно верхнего уровня(TOP_LEVEL_WINDOW), которое включает в себя дочерний ....
         CW_USEDEFAULT,CW_USEDEFAULT, //Начальная позицция окна
-        CW_USEDEFAULT,CW_USEDEFAULT, //Размер окна
+        newWidth, newHeight, //Размер окна
         NULL, //Parent Window
         NULL, //Для главного окна это меню. Для дочернего окна это ID его ресурса (IDC_EDIT)
         hInstance,
@@ -92,6 +98,37 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     case WM_COMMAND:
         break;
     case WM_DESTROY: PostQuitMessage(0); break;
+    case WM_SIZE:
+    {
+       
+        // Получить новые размеры окна
+        int width = LOWORD(lParam);
+        int height = HIWORD(lParam);
+        CONST INT SIZE = 256;
+        CHAR szTitle[SIZE] = {};
+
+        // Обновить заголовок окна с новыми размерами
+        sprintf(szTitle,  "%s - %dx%d", g_sz_MY_WINDOW_CLASS, width, height);
+        SetWindowText(hwnd, szTitle);
+
+        break;
+    }
+    case WM_MOVE:
+    {
+        // Получить новое положение окна
+        int x = LOWORD(lParam);
+        int y = HIWORD(lParam);
+        RECT windowRect;
+
+        // Обновить заголовок окна с новыми координатами
+        CONST INT SIZE = 256;
+        CHAR szTitle[SIZE] = {};
+        GetWindowRect(hwnd, &windowRect);
+        sprintf(szTitle, "%s - %dx%d - x:%d, y:%d", g_sz_MY_WINDOW_CLASS, windowRect.right - windowRect.left, windowRect.bottom - windowRect.top, x, y);
+        SetWindowText(hwnd, szTitle);
+
+        break;
+    }
     case WM_CLOSE: 
         if(MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Question", MB_YESNO | MB_ICONQUESTION) == IDYES)
             DestroyWindow(hwnd); 
