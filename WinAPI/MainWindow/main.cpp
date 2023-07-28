1bf1bfe004196e4ded115e1be275782238ac7ebb
Alikhanov Ibragim N <a01141226993n@gmail.com>
2023-07-28 14:17:35 +0300

    HW calculator is improved

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -1,4 +1,4 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿﻿#define _CRT_SECURE_NO_WARNINGS
 #include<Windows.h>
 #include<cstdio>
 #include"resource.h"
@@ -7,8 +7,8 @@ CONST CHAR g_sz_MY_WINDOW_CLASS[] = "MyFirstWindow";
 //g_ - Global, i - Int 
 CONST INT g_i_BTN_SIZE = 50;	//размер кнопки
 CONST INT g_i_DISTANCE = 10;	//Расстояние между кнопками
-CONST INT g_i_START_X = 10;		//Отступ от начала окна
-CONST INT g_i_START_Y = 10;		//Отступ от начала окна
+CONST INT g_i_START_X = 10;	//Отступ от начала окна
+CONST INT g_i_START_Y = 10;	//Отступ от начала окна
 CONST INT g_i_DISPLAY_WIDTH = (g_i_BTN_SIZE * 5 + g_i_DISTANCE * 4);
 CONST INT g_i_DISPLAY_HEIGHT = 18;
 
@@ -95,16 +95,17 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	//SetFocus(hwnd);
 	switch (uMsg)
 	{
 	case WM_CREATE:
 	{
 		HWND hEdit = CreateWindowEx
 		(
-			NULL, 
+			NULL, //exStyle
 			"Edit",
 			"0",
-			WS_CHILD | WS_VISIBLE | ES_RIGHT | WS_BORDER,
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT | ES_READONLY,
 			g_i_START_X, g_i_START_Y,
 			g_i_DISPLAY_WIDTH, g_i_DISPLAY_HEIGHT,
 			hwnd, (HMENU)IDC_EDIT, GetModuleHandle(NULL), NULL
@@ -115,6 +116,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			for (int j = 0; j < 3; j++)
 			{
+				//https://learn.microsoft.com/en-us/windows/win32/controls/button-styles
 				sz_btn_name[0] = number + 48;
 				CreateWindowEx
 				(
@@ -122,7 +124,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 					"Button",
 					sz_btn_name,
 					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
-					g_i_START_X  + (g_i_BTN_SIZE + g_i_DISTANCE) * j,
+					g_i_START_X /*+ g_i_DISTANCE*/ + (g_i_BTN_SIZE + g_i_DISTANCE) * j,
 					g_i_START_Y + g_i_DISTANCE + (g_i_BTN_SIZE + g_i_DISTANCE) * (2 - i) + g_i_DISPLAY_HEIGHT,
 					g_i_BTN_SIZE, g_i_BTN_SIZE,
 					hwnd,
@@ -232,6 +234,8 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		GetWindowRect(hwnd, &rect);	//Получаем прямоугольник окна
 		//&rect - взятие адреса в памяти 'rect'
 
+		//INT start_x = rect.left;
+		//INT start_y = rect.top;
 		INT width = rect.right - rect.left;
 		INT height = rect.bottom - rect.top;
 
@@ -244,17 +248,23 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	break;
 	case WM_COMMAND:
 	{
+		//SendMessage(hwnd, WM_SETFOCUS, NULL, NULL);
+		SetFocus(hwnd);
 		CONST INT SIZE = 256;
 		CHAR sz_buffer[SIZE] = {};
 		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
 		CHAR sz_digit[2] = {};
 		static double a = 0;
-		double b = 0;
+		static double b = 0;
 		static bool stored = false;
+		static bool input = false;
+		static bool operation_input = false;
 		static char operation = 0;
+		//static char old_operation = 0;
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
 		{
-			if (stored && operation != 0)
+			input = true;
+			if (stored)
 			{
 				SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
 				stored = false;
@@ -280,17 +290,25 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
 		{
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"0");
-			a = 0;
+			a = b = 0;
 			stored = false;
+			input = false;
+			operation_input = false;
+			operation = 0;
+			//SendMessage(hwnd, WM_SETFOCUS, NULL, NULL);	//DOESN'T WORK
+			//SetFocus(hwnd);
 		}
 		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
 		{
 			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
-			b = strtod(sz_buffer, NULL);
+			if (input)b = strtod(sz_buffer, NULL);
 			if (a == 0)
-			{
 				a = b;
-				break;
+			stored = true;
+			input = false;
+			if (/*operation == old_operation &&*/ operation_input)
+			{
+				SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_EQUAL, 0);
 			}
 			switch (LOWORD(wParam))
 			{
@@ -299,10 +317,14 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			case IDC_BUTTON_ASTER:	operation = '*'; break;
 			case IDC_BUTTON_SLASH:	operation = '/'; break;
 			}
-			stored = true;
+			operation_input = true;
 		}
 		if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
 		{
+			SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			if (input)
+				b = strtod(sz_buffer, NULL);
+			input = false;
 			switch (operation)
 			{
 			case '+': a += b; break;
@@ -310,14 +332,59 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			case '*': a *= b; break;
 			case '/': a /= b; break;
 			}
+			//old_operation = operation;
+			operation_input = false;
 			sprintf(sz_buffer, "%g", a);
 			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
 	}
 	break;
+	case WM_KEYDOWN:
+	{
+		//char symbol[2]{};
+		//symbol[0] = LOWORD(wParam);
+		//MessageBox(hwnd, symbol, "Symbol", MB_OK);
+
+		if (GetKeyState(VK_SHIFT) < 0)
+		{
+			if (wParam == 0x38)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0);
+		}
+		else
+		{
+			if (wParam >= 0x30 && wParam <= 0x39)
+				SendMessage(hwnd, WM_COMMAND, wParam - 0x30 + 1000, 0);
+
+		}
+		if (wParam >= 0x60 && wParam <= 0x69)
+			SendMessage(hwnd, WM_COMMAND, wParam - 0x60 + 1000, 0);
+
+		//if(GetKeyState(0x38) & 0x8000)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0);
+		switch (LOWORD(wParam))
+		{
+		case VK_OEM_PLUS:	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_PLUS, 0);  break;
+		case VK_OEM_MINUS:	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_MINUS, 0); break;
+			//case 0x09:
+		case VK_MULTIPLY:	SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0); break;
+		case VK_OEM_2:
+		case VK_DIVIDE:		SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_SLASH, 0); break;
+		case VK_RETURN:		SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_EQUAL, 0); break;
+		}
+		//if (GetKeyState(VK_SHIFT) & 0x8000 && GetKeyState(0x38) & 0x8000)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_ASTER, 0);
+
+		if (wParam == VK_OEM_PERIOD)SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_POINT, 0);
+
+		/*if (wParam >= 0x30 && wParam <= 0x39)
+		{
+			SendMessage(hwnd, WM_COMMAND, wParam - 0x30 + 1000, 0);
+		}*/
+
+	}
+	break;
 	case WM_DESTROY: PostQuitMessage(0); break;
 	case WM_CLOSE:
-		DestroyWindow(hwnd); 
+		//if (MessageBox(hwnd, "Вы действительно хотите закрыть окно?", "Question", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		DestroyWindow(hwnd); //Функция DestroyWindow() уничтожает окно
+		//Конкретно здесь, функция DestroyWindow() посылает окну вообщение WM_DESTROY
 		break;
 	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
