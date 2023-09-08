506a0e48e71f97fc0d97a00a701c0a2d696e8333
Alikhanov Ibragim N <a01141226993n@gmail.com>
2023-09-08 16:41:02 +0300

    HW is DONE

---
+++ WPF/ListView/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Windows;
+using System.Collections;
+
+namespace ListView
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			lvEntries.Items.Add("Хорошо");
+			lvEntries.Items.Add("живет");
+			lvEntries.Items.Add("на");
+			lvEntries.Items.Add("свете");
+			lvEntries.Items.Add("Винни");
+			lvEntries.Items.Add("Пух");
+		}
+
+		private void BtnAdd_Click(object sender, RoutedEventArgs e)
+		{
+			lvEntries.Items.Add(txtEntry.Text);
+			txtEntry.Clear();
+		}
+
+		private void BtnClr_Click(object sender, RoutedEventArgs e)
+		{
+			lvEntries.Items.Clear();
+		}
+
+		private void BtnDel_Click(object sender, RoutedEventArgs e)
+		{
+			//object item = lvEntries.SelectedItem;
+			//if
+			//	(
+			//		MessageBox.Show
+			//		(
+			//		"Вы действительно собираетесь удавить выбранный элемент",
+			//		"Question",
+			//		MessageBoxButton.YesNo,
+			//		MessageBoxImage.Question
+			//		) == MessageBoxResult.Yes
+			//	)
+			//{
+			//	lvEntries.Items.Remove(item);
+			//}
+
+			System.Collections.IList items = lvEntries.SelectedItems;
+			if
+				(
+					MessageBox.Show
+					(
+						"Вы действительно хотите удавить выбранные элементы",
+						"Question",
+						MessageBoxButton.YesNo,
+						MessageBoxImage.Question
+					) == MessageBoxResult.Yes
+				)
+			{
+				System.Collections.ArrayList itemsList = new ArrayList(items);
+				foreach (string item in itemsList)
+				{
+					lvEntries.Items.Remove(item);
+				}
+			}
+		}
+	}
+}
---
+++ WPF/ObservableCollection/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace ListView
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			DataContext = this;
+			Entries = new ObservableCollection<string>(new string[] { "No", "money", "no", "honey" });
+			InitializeComponent();
+		}
+
+		private ObservableCollection<string> entries;
+
+		public ObservableCollection<string> Entries
+		{
+			get { return  entries; }
+			set
+			{
+				entries = value;
+			}
+		}
+
+
+		private void BtnAdd_Click(object sender, RoutedEventArgs e)
+		{
+			Entries.Add(txtEntry.Text);
+			txtEntry.Clear();
+			txtEntry.Focus();
+		}
+
+		private void BtnClr_Click(object sender, RoutedEventArgs e)
+		{
+			Entries.Clear();
+		}
+
+		private void BtnDel_Click(object sender, RoutedEventArgs e)
+		{
+			Entries.Remove((string)lvEntries.SelectedItem);
+		}
+	}
+}
